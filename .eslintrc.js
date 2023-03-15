4140a0d57e9a422c137acc1428c0a0bb29b88b43
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-03-15 11:49:26 +0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard", "plugin:import/errors", "plugin:import/warnings"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "comma-dangle": 0,
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "max-len": [
+      "error",
+      100,
+      {
+        ignoreStrings: true,
+      },
+    ],
+    "no-alert": "off",
+    "no-console": "off",
+    "no-debugger": "error",
+    "no-param-reassign": "off",
+    "import/prefer-default-export": "off",
+    "max-classes-per-file": ["error", 2],
+    "no-restricted-exports": "off",
+    "import/no-unused-modules": "error",
+  },
+};
--- src/www.js
+++ src/www.js
@@ -34,7 +34,7 @@ server.on("listening", onListening);
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
+function normalizePort (val) {
   const port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -54,7 +54,7 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+function onError (error) {
   if (error.syscall !== "listen") {
     throw error;
   }
@@ -80,7 +80,7 @@ function onError(error) {
  * Event listener for HTTP server "listening" event.
  */
 
-function onListening() {
+function onListening () {
   const addr = server.address();
   const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
   debug("api:server")("Listening on " + bind);
