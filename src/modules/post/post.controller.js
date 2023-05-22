65da95b5d72d7143cef2aaf4fb9bc7cae0629d7c
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-05-22 09:44:40 +0700

    feat: get one post by slug

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -1,8 +1,8 @@
 import httpStatus from "http-status";
 
-import PostService from "./post.service";
-import { isEmpty } from "@/utils";
 import APIError from "@/error/APIError";
+import { isEmpty } from "@/utils";
+import PostService from "./post.service";
 
 class PostController {
   async createPost(req, res, next) {
@@ -30,6 +30,26 @@ class PostController {
   async getOne(req, res, next) {
     try {
       const post = await PostService.getOne(req.params.id);
+
+      if (isEmpty(post)) {
+        return next(
+          APIError.NotFound({
+            message: "Post not found",
+          })
+        );
+      }
+
+      return res.status(httpStatus.OK).json({
+        data: post,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getOneBySlug(req, res, next) {
+    try {
+      const post = await PostService.getOneBySlug(req.params.slug);
 
       if (isEmpty(post)) {
         return next(
