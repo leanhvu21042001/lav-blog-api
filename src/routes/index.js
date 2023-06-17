a03dece1f5689e6e25a6414a2f24d73a897bc960
leanhvu21042001 <leanhvu21042001@gmail.com>
2023-06-17 09:08:48 +0700

    fix: refactor folder structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 
-import postRoutes from "@/modules/post/post.routes";
+import admin from "@/modules/admin";
+import client from "@/modules/client";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => res.json({ message: "App is running" }));
-router.use("/post", postRoutes);
+
+// load routes for client.
+router.use("/", client);
+// lad routes for admin.
+router.use("/admin", admin);
 
 export default router;
